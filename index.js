445ac06f7ffc7d48986bb37e705cc47c8d52a765
k-a-r-o-x-l-a-d-e <oudedibor4@gmail.com>
2020-05-27 05:44:34 -0700

    m

---
+++ index.js
@@ -0,0 +1,15 @@
+var parent = document.getElementById("li_of_customers");
+
+var i;
+var x;
+for(i=10; i>0; i--){
+    console.log(i+" "+x);
+   
+        
+    // var child = document.createElement("li");
+    // child.innerHTML="yolo"
+    }
+    for(x=0; x<10; x++){
+        console.log("how are you");
+    // parent.appendChild(child);
+}
---
+++ ss.js
@@ -0,0 +1,6 @@
+var x="otito";
+var person={
+    name :x
+};
+alert("person");
+var formTable = [];
